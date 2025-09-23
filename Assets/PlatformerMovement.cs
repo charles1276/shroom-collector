32dcfd20d9003adc2e7d7632d893371037d70a5b
MUKILTEO\1725289 <charles82508@gmail.com>
2025-09-23 10:20:37 -0700

    player

--- Assets/PlatformerMovement.cs
+++ Assets/PlatformerMovement.cs
@@ -1,65 +1,60 @@
-
 using System.Collections;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Windows;
 using Input = UnityEngine.Input;
 
 public class PlatformerMovement : MonoBehaviour
 {
     [SerializeField] private Animator animator;
-
+    [SerializeField] private TrailRenderer tr;
     public float moveSpeed;
     public float jumpHeight;
-    
 
-    public float dashSpeed = 20f;
-    public float dashTime = 0.2f;
-    public float dashCooldown = 1f;
+    private float dashSpeed = 56f;
+    private float dashTime = 0.4f;
+    private float dashCooldown = 1f;
     private bool canDash = true;
-    private bool isDashing = false;
-    private float dashTimer;
+    private bool isDashing;
 
     private Rigidbody2D rb2d;
-
     private float _movement;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        if (tr != null)
+            tr.emitting = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
         {
- 
-            Coroutine dashCoroutine = StartCoroutine(Dash());
+            StartCoroutine(Dash());
         }
 
-        rb2d.linearVelocityX = _movement;
-        if (rb2d.linearVelocityX > 0)
+        // Only allow normal movement if not dashing
+        if (!isDashing)
+        {
+            rb2d.linearVelocity = new Vector2(_movement, rb2d.linearVelocity.y);
+        }
+
+        if (rb2d.linearVelocity.x > 0)
         {
             animator.SetInteger("walkdirction", +1);
             GetComponent<SpriteRenderer>().flipX = false;
         }
-        else if (rb2d.linearVelocityX < 0)
+        else if (rb2d.linearVelocity.x < 0)
         {
-
             animator.SetInteger("walkdirction", -1);
             GetComponent<SpriteRenderer>().flipX = true;
         }
         else
         {
             animator.SetInteger("walkdirction", 0);
-
         }
 
-        if (rb2d.linearVelocityY != 0)
+        if (rb2d.linearVelocity.y != 0)
         {
             animator.SetBool("isJumping", true);
         }
@@ -69,36 +64,42 @@ public class PlatformerMovement : MonoBehaviour
         }
     }
 
-    
     public void Move(InputAction.CallbackContext ctx)
     {
         _movement = ctx.ReadValue<Vector2>().x * moveSpeed;
-
     }
+
     public void Jump(InputAction.CallbackContext ctx)
     {
         if (ctx.ReadValue<float>() == 1)
         {
-            rb2d.linearVelocityY = jumpHeight;
-
-
-
+            rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, jumpHeight);
         }
     }
-    private IEnumerator Dash()
+
+    private IEnumerator Dash()  
     {
         canDash = false;
         isDashing = true;
         float originalGravity = rb2d.gravityScale;
         rb2d.gravityScale = 0;
-        rb2d.linearVelocity = new Vector2(transform.localScale.x * dashSpeed, 0);
-        yield return new WaitForSeconds(dashTime);
-        rb2d.gravityScale = originalGravity;
 
+        if (tr != null)
+            tr.emitting = true;
+
+        float dashDir = _movement != 0 ? Mathf.Sign(_movement) : 1f;
+        rb2d.linearVelocity = new Vector2(dashDir * dashSpeed, 0);
+
+        yield return new WaitForSeconds(dashTime);
+
+        rb2d.gravityScale = originalGravity;
         isDashing = false;
+
+        if (tr != null)
+            tr.emitting = false;
+
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
     }
-
 }
 //linkfor dash vidieohttps://www.youtube.com/watch?v=yB6ty0Gj7tA
