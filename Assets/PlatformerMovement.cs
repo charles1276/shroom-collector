947b7d6bc5393f81c905213e9b457b0a6029599a
MUKILTEO\1725289 <charles82508@gmail.com>
2025-09-23 08:46:38 -0700

    change direction

--- Assets/PlatformerMovement.cs
+++ Assets/PlatformerMovement.cs
@@ -30,64 +30,35 @@ public class PlatformerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //dash left
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            if (doubleTapTime> Time.time && lastKeycode == KeyCode.A)
-            {
-                StartCoroutine(Dash(-1f));
-            }
-            else
-            {
-                doubleTapTime = Time.time + 0.2f;
-            }
-            lastKeycode = KeyCode.A;
-            //dash right
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                if (doubleTapTime > Time.time && lastKeycode == KeyCode.D)
-                {
-                    StartCoroutine(Dash(1f));     
-                }
-                else
-                {
-                    doubleTapTime = Time.time + 0.5f;
-                }
-                lastKeycode = KeyCode.D;
-        }
         rb2d.linearVelocityX = _movement;
         if (rb2d.linearVelocityX > 0)
-            {
-                animator.SetBool("isWalking", true);
-            }
+        {
+            animator.SetInteger("walkdirction", +1);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
         else if (rb2d.linearVelocityX < 0)
-            {
+        {
 
-                animator.SetBool("isWalkingLeft", true);
-            }
+            animator.SetInteger("walkdirction", -1);
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
         else
-            {
-                animator.SetBool("isWalking", false);
+        {
+            animator.SetInteger("walkdirction", 0);
 
-            }
+        }
 
         if (rb2d.linearVelocityY != 0)
-            {
-                animator.SetBool("isJumping", true);
-            }
-        else
-            {
-                animator.SetBool("isJumping", false);
-            }
-        } }
-
-    private void FixedUpdate()
-    {
-        if (!isDashing)
         {
-            rb2d.linearVelocity = new Vector2(_movement * moveSpeed, rb2d.linearVelocity.y);
+            animator.SetBool("isJumping", true);
+        }
+        else
+        {
+            animator.SetBool("isJumping", false);
         }
     }
+
+    
     public void Move(InputAction.CallbackContext ctx)
     {
         _movement = ctx.ReadValue<Vector2>().x * moveSpeed;
@@ -103,20 +74,7 @@ public class PlatformerMovement : MonoBehaviour
 
         }
     }
-    IEnumerator Dash(float direction)
-    {
-        isDashing = true;
-       
-        rb2d.linearVelocity = new Vector2(rb2d.linearVelocity.x, 0f);
-      
-        rb2d.AddForce(new Vector2(dashDistance * direction, 0f), ForceMode2D.Impulse);
-        float gravity= rb2d.gravityScale;
-        rb2d.gravityScale = 0f;
-        yield return new WaitForSeconds(0.2f);
-        isDashing = false;
-        rb2d.gravityScale = gravity;
-        rb2d.gravityScale = 5f;
-    }
+    
 
 }
 //linkfor dash vidieohttps://www.youtube.com/watch?v=yB6ty0Gj7tA
